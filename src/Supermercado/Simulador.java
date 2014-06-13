4c21acf4e699f2babfec29eafd51ae6c8e51a007
TIAGO <TIAGO@TIAGO-VAIO>
2014-06-12 23:48:44 -0300

    4

---
+++ src/Supermercado/Simulador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Supermercado;
+
+/**
+ *
+ * @author TIAGO
+ */
+public interface Simulador {
+    public void imprimirResultados();
+    public void limpar();
+    public void simular
+    public String NumeroDeAtendimentos(int a);
+    public double tempoMedioAtendimento(int a);
+    public int utilizacao(int a);
+    public int MaximoFila(int a);
+    public double mediaTempoDeEspera(int a);
+    public int atendimentosSemEspera(int a);
+    public int tempoFilaVazia(int a);
+    
+    
+    
+    
+    
+}
